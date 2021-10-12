1b15f1e645b1b4badc5792435371aa906149e72c
prasadacharya26 <68582203+prasadacharya26@users.noreply.github.com>
2021-10-12 21:40:12 +0530

    Add files via upload

---
+++ Array/Week.java
@@ -0,0 +1,7 @@
+class Week{
+	public static void main(String[] args){
+		String[] days={"Sunday","Monday","Wednesday","Thursday","Friday","Saturday"};
+		byte num=4;
+		System.out.println(days[num-1]);
+	}
+}
---
+++ Array/Year.java
@@ -0,0 +1,7 @@
+class Year{
+	public static void main(String[] args){
+		String[] month={"January","February","March","April","May","June","July","August","September","October","November","Decmber"};
+		byte num=1;
+		System.out.println(month[num-1]);
+	}
+}
