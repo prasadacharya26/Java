7773e7a62b2052476dd2af13438f8a36d1d30966
prasadacharya26 <68582203+prasadacharya26@users.noreply.github.com>
2021-10-20 21:47:28 +0530

    Add files via upload

---
+++ Hibernate/butterfly/src/main/java/com/wolken/butterfly/entities/ButterflyDetails.java
@@ -0,0 +1,65 @@
+package com.wolken.butterfly.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="butterflyDetails")
+public class ButterflyDetails {
+	@Id
+	@Column(name="id")
+	private int id;
+	@Column(name="name")
+	private String name;
+	@Column(name="marking")
+	private String marking;
+	@Column(name="color")
+	private String color;
+	@Column(name="size")
+	private String size;
+	@Column(name="weight")
+	private float weight;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMarking() {
+		return marking;
+	}
+	public void setMarking(String marking) {
+		this.marking = marking;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	@Override
+	public String toString() {
+		return "ButterflyDetails [id=" + id + ", name=" + name + ", marking=" + marking + ", color=" + color + ", size="
+				+ size + ", weight=" + weight + "]";
+	}
+}
