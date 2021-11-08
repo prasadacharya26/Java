04031c901efada89cbfe04e96c570526d84312e9
prasadacharya26 <68582203+prasadacharya26@users.noreply.github.com>
2021-11-08 23:34:18 +0530

    Add files via upload

--- Spring/Dependency-Injection/MVC/Monuments/src/main/java/com/wolken/monumnet/entity/MonumentEntity.java
+++ Spring/Dependency-Injection/MVC/Monuments/src/main/java/com/wolken/monumnet/entity/MonumentEntity.java
@@ -1,5 +1,25 @@
 package com.wolken.monumnet.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
 public class MonumentEntity {
-	
+	@Id
+	private int id;
+	private String name;
+	private int area;
+	private int height;
+	private String architect;
 }
